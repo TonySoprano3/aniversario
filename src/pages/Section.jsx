8ce972acd7615bbd0e2da5626678e8d87cabc45d
TonySoprano3 <lautarovirtual@hotmail.com>
2022-12-19 20:15:50 -0300

    kirby

--- src/pages/Section.jsx
+++ src/pages/Section.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import '../styles/Section.scss'
+import Kirby from '../assets/Kirby.jpg'
 const Section = () => {
   return (
-    <div>Section</div>
+    <section className='section'>
+      <img src={Kirby} alt="Kirby" />
+   <p>
+    Nadie sabe que va a pasar después. Así que te dejo una página llena de Kirby's. Esperemos que sea otro año bueno, con vos eso es seguro, pero bueno, nunca están de más unos Kirby's
+    </p>
+    </section>
+   
+
   )
 }
 
