4616797e0e3ed744add6193dc5dc5f6d258aca73
TonySoprano3 <lautarovirtual@hotmail.com>
2022-12-19 18:17:21 -0300

    changes3

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.scss'
-import { Home, Story, NotFound, Dedication, End, Section } from './pages'
+import { Home, NotFound, Dedication, End, Section } from './pages'
 import { Navbar } from './components'
-
+const LazyAbout = React.lazy(() => import('../src/pages/Story'))
 
 function App() {
 
@@ -13,8 +14,13 @@ function App() {
      <Routes >
 
       <Route path='/' element={<Home/>}/>
-      <Route path='/historia' element={<Story/>}/>
-      <Route path='/continuacion'  element={<Section/>}/>
+      <Route path='/historia' element={
+          <React.Suspense fallback='Loading...'>
+          <LazyAbout/>  
+          </React.Suspense>
+    }/>
+      <Route path='/continuacion'  element={
+      <Section/>}/>
       <Route path='/dedicatoria' element={<Dedication/>}/>
       <Route path='/final' element={<End/>}/>
       <Route path='*' element={<NotFound/>}/>
