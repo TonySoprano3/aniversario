97b61739da52603c30cf647d04368e2acc1f0e0f
TonySoprano3 <lautarovirtual@hotmail.com>
2022-12-20 14:13:53 -0300

    animate

--- src/pages/Section.jsx
+++ src/pages/Section.jsx
@@ -2,14 +2,28 @@ import React from 'react'
 import '../styles/Section.scss'
 import Kirby from '../assets/Kirby.jpg'
 import BgKirby from '../assets/anniversary-bg.jpg'
+import { motion } from 'framer-motion'
+
 const Section = () => {
   return (
-    <section className='section'>
-      <img src={Kirby} alt="Kirby" />
-   <p>
+    <motion.section className='section'
+    initial={{opacity:0}}
+    animate={{opacity:1}}
+    transition={{ duration:1 }}
+    >
+      <motion.img src={Kirby} alt="Kirby" 
+      initial={{ translateX: -1000 }}
+      animate={{ translateX: 0, rotate: 360 }}
+      transition={{ duration: 1.5 }}
+      />
+   <motion.p
+   initial={{ translateX: 1000 }}
+   animate={{ translateX: 0 }}
+   transition={{ duration: 1.5 }}
+   >
     Nadie sabe que va a pasar después. Así que te dejo una página llena de Kirby's. Esperemos que sea otro año bueno, con vos eso es seguro. Nunca están de más unos Kirby's
-    </p>
-    </section>
+    </motion.p>
+    </motion.section>
    
 
   )
