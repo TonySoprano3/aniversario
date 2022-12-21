1688e3c8081986585d75e5150a23d52261af7d7c
TonySoprano3 <lautarovirtual@hotmail.com>
2022-12-21 16:00:03 -0300

    comentario1

--- src/pages/Dedication.jsx
+++ src/pages/Dedication.jsx
@@ -8,7 +8,7 @@ const Dedication = () => {
       <img src={Kirbys} alt="Kirbys"  />
       <article className='dText'>
       <h1>Te dedico</h1><hr/>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, accusamus tempora explicabo dolorum unde culpa officiis. Sed iusto quidem enim ullam consequatur quae sint, qui harum facilis voluptatum magnam aliquam.</p>
+      <p>Esta aplicación, que aprendí bastante cosas y me estrese bastante haciendola, porque siento que es insuficiente y no es digna de vos. Perdón por no ser tan bueno, pero créeme que lo intento. Por eso te la dedico, estas páginas que estuve haciendo estos días, pensnado en qué podría gustarte más o menos. Seguramente te hubiese gustado otra cosa pero, siempre te he regalado cosas materiales, nunca te di algo hecho con mis propias manos, y esto es lo que mejor sé hacer por ahora. Cada mejora que tenga vas a verla reflejada en nosotros.</p>
       </article>
     </div>
   )
