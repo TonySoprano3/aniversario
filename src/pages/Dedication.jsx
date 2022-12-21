fda558bec22110a02ef013af2932ad9aab9661d0
TonySoprano3 <lautarovirtual@hotmail.com>
2022-12-21 13:53:18 -0300

    comit

--- src/pages/Dedication.jsx
+++ src/pages/Dedication.jsx
@@ -4,7 +4,12 @@ import Kirbys from '../assets/Kirbys.png'
 const Dedication = () => {
   return (
     <div className='dedication'>
+    
       <img src={Kirbys} alt="Kirbys"  />
+      <article className='dText'>
+      <h1>Te dedico</h1><hr/>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, accusamus tempora explicabo dolorum unde culpa officiis. Sed iusto quidem enim ullam consequatur quae sint, qui harum facilis voluptatum magnam aliquam.</p>
+      </article>
     </div>
   )
 }
